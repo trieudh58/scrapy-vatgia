ef7dc52e61b36558f8772f11c37d4ad1512f0a35
trieudh <trieudh@icts.vn>
2016-08-04 22:42:57 +0700

    update

--- lamchame/spiders/lamchame_spider.py
+++ lamchame/spiders/lamchame_spider.py
@@ -4,51 +4,57 @@ from lamchame.items import LamChaMeItem
 
 class LCMSpider(scrapy.Spider):
     name = 'lamchame'
-    allowed_domains = ['lamchame.com']
+    allowed_domains = ['vatgia.com']
 
-    # truong lop hoc hanh
-    # root_url = 'https://www.lamchame.com/forum/forums/truong-lop-hoc-hanh.38/'
+    # chototvn - vat nuoi thu cung
+    # root_url = 'https://www.chotot.com/ha-noi/mua-ban-vat-nuoi-thu-cung'
 
-    # tieng anh cho con
-    # root_url = 'https://www.lamchame.com/forum/forums/tieng-anh-cho-con.322/'
+    # san pham khac
+    # root_url = 'https://www.chotot.com/ha-noi/mua-ban-cac-loai-khac'
 
-    # tai chinh gia dinh
-    root_url = 'https://www.lamchame.com/forum/forums/tai-chinh-gia-dinh.125/'
+    # du lich
+    # root_url = 'https://www.chotot.com/toan-quoc/du-lich'
+
+    # the thao
+    # root_url = 'https://www.chotot.com/toan-quoc/mua-ban-do-the-thao-da-ngoai'
+
+    # phu kien thoi tr
+    # root_url = 'hatgiattps://www.chotot.com/toan-quoc/mua-ban-giay-dep-tui-xach-phu-kien'
+
+    root_url = 'http://www.vatgia.com/raovat/type.php?iCat=3794'
 
     start_urls = []
 
     url = ''
 
-    # truong lop hoc hanh
-    # for i in range(0, 342):
-
-    # tieng anh cho con
-    for i in range(0, 179):
-        url = root_url + 'page-' + str(i)
+    for i in range(2, 14):
+        url = root_url + '&page=' + str(i)
         start_urls.append(url)
 
     def parse(self, response):
-        for href in response.xpath('//a[@class="PreviewTooltip"]/@href'):
+        for href in response.xpath('//h2/a[@class="raovat_name tooltip"]/@href'):
             url = response.urljoin(href.extract())
             yield scrapy.Request(url, callback=self.parse_dir_contents)
 
-        # file_name = response.url.split('/')[-2] + '.html'
-        # with open(file_name, 'wb') as f:
-        #     f.write(response.body)
-
     def parse_dir_contents(self, response):
-        for sel in response.xpath('//div[@class="messageContent"]/article'):
-            body = ''
-            arr = map(unicode.strip, sel.xpath('blockquote[@class="messageText SelectQuoteContainer ugc baseHtml"]/text()').extract())
-
-            for x in arr:
-                if x != '' and x != ' ':
-                    x = x.rstrip('\n')
-                    body += x
-
-            if len(body) >= 10:
-                item = LamChaMeItem()
-                item['post_body'] = body
-                yield item
-            else:
+        # for sel in response.xpath('//span'):
+        sel = response.xpath('//span')
+        body = ''
+        arr = map(unicode.strip, sel.xpath('span/text()').extract())
+        # arr = sel.xpath('span/text()').extract()
+        for x in arr:
+            # x = map(unicode.strip, x)
+            if '(8h - 21h' in x:
+                arr.remove(x)
                 continue
+            if x != '' and x != ' ':
+                x = x.replace('\n', ' ')
+                x = x.replace('\t', '')
+                body += x
+
+        # print body
+
+        if 10 < len(body) < 3000:
+            item = LamChaMeItem()
+            item['post_body'] = body
+            yield item
